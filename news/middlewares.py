2cbf676fdb57f1ac035d25a29fe078678b8a7df2
caiopo <caio@bridge.ufsc.br>
2019-10-22 10:28:05 -0300

    Create new project

--- news/middlewares.py
+++ news/middlewares.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 # Define here the models for your spider middleware
 #
 # See documentation in:
@@ -6,7 +8,7 @@
 from scrapy import signals
 
 
-class ReviewsSpiderMiddleware(object):
+class NewsSpiderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -54,7 +56,7 @@ class ReviewsSpiderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
-class ReviewsDownloaderMiddleware(object):
+class NewsDownloaderMiddleware(object):
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
